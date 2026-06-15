778f7a54b33d9fe4b1fd7d82ecb48d695552f9b9
Phil <igrfilatv200@rambler.ru>
2026-06-15 16:26:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+/* let message = "Третий скрипт";
+console.log(message)
+
+let a = 10;
+console.log(a + 3);
+
+let b;
+b = 5;
+console.log(b);
+b = "hello";
+console.log(b);
+
+let name = "Игорь";
+let camelCase = "";
+let snake_case = "";
+console.log(name); */
+
+// const b = 5;
+// console.log(b);
+// b = "hello";
+// console.log(b);
+/* 
+let a = "hello";
+console.log(a, typeof(a));
+let b = 'world';
+console.log(b, typeof(b));
+let c = `Обратные кавычки`;
+console.log(c, typeof(c)); */
+
+/* let firstName = "Sergey"
+alert(`hello, ${firstName}`); */
+
+
+let day = 365;
+let planeta = "Земля";
+let people = "7 млрд";
+let star = "Солнца";
+
+
+alert (message = "Мы живем на планете " + planeta + ", она делает один оборот вокруг " + star + " за " + day + " дней. Население нашей планеты составляет примерно " + people + " человек.");
+console.log(message);
